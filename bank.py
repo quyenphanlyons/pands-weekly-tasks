8637d06603c912cca58a1d14abe99e18cc3e0ed0
quyenphanlyons <quyenphanlyons@gmail.com>
2025-03-05 23:45:17 +0000

    improve2

--- bank.py
+++ bank.py
@@ -1,4 +1,6 @@
 #Ask user to put 2 amounts in cent
 a1,a2=input("Enter 2 amounts in cent: ").split() #function input source geeksforgeeks.org
+a1=int(a1) #a1 is a number
+a2=int(a2) #a2 is a number
 sum=a1+a2
 print('€',sum) #print out this sum with the currency in front
