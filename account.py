5cd83f28d40b6990a756808498ab90201d3224e8
quyenphanlyons <quyenphanlyons@gmail.com>
2025-03-07 10:58:59 +0000

    account improve2

--- account.py
+++ account.py
@@ -6,6 +6,8 @@
 
 account = int(input('Please enter 10 digit of an account number: ')) # variable account can only contains numbers
 account_str = str(account)
-display = account_str.replace(account_str[0:6],'XXXXXX')
-print(display)
-
+n = len(account_str)
+if n != 10:
+    print('An account number must contains 10 digits!')
+else: display = account_str.replace(account_str[0:6],'XXXXXX')
+print("User's account number is {}".format(display))
