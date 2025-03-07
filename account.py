55ef6ba481caa208191e253e4020e4fabe0effe8
quyenphanlyons <quyenphanlyons@gmail.com>
2025-03-07 11:38:30 +0000

    account improve3

--- account.py
+++ account.py
@@ -2,12 +2,15 @@
 #Please enter an 10 digit account number
 #only display the last 4 characters (with the other other characters replaced with Xs)
 
-#TO DO: fix the number of account to 10
-
 account = int(input('Please enter 10 digit of an account number: ')) # variable account can only contains numbers
 account_str = str(account)
+display = account_str.replace(account_str[0:6],'XXXXXX')
 n = len(account_str)
-if n != 10:
-    print('An account number must contains 10 digits!')
-else: display = account_str.replace(account_str[0:6],'XXXXXX')
-print("User's account number is {}".format(display))
+correctlen=10
+
+#Only display the account number when it contains 10 digits
+#If else send an error message
+if n == correctlen:
+    print("User's account number is {}".format(display))
+else: 
+    print('This is not a valid number')
