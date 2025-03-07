bf8e26d7f746ad630ea885a5bd3fd94faf9d72e7
quyenphanlyons <quyenphanlyons@gmail.com>
2025-03-07 22:35:39 +0000

    collatz improve1

--- collatz.py
+++ collatz.py
@@ -2,13 +2,15 @@
 #At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
 
 x = int(input("Enter a positive number: "))
-list = [x]
+list=[x]
 
-if x%2==0:
-    x = x/2
-    list = list.append(x)
-else: x = x*3+1
-    list = list.append(x)
+while x!=1:
+    if x%2==0:        
+        x = x/2
+        list.append(x)
+    else: 
+        x = x*3+1
+        list.append(x)
 
 print(list)
 
