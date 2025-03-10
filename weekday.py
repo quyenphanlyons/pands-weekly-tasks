9a84768be669991e10a56588871028bd18cf4e4e
quyenphanlyons <quyenphanlyons@gmail.com>
2025-03-10 20:17:42 +0000

    weekday first code

---
+++ weekday.py
@@ -0,0 +1,18 @@
+# Write a program that outputs whether or not today is a weekday
+
+# Find out what day it is
+from datetime import date
+import calendar
+my_date = date.today()
+today = calendar.day_name[my_date.weekday()]
+
+weekday = ("Monday","Tuesday","Wednesday","Thursday","Friday")
+weekend = ("Saturday","Sunday")
+
+for today in weekday:
+    print("Yes, unfortunately today is a weekday.")
+
+for today in weekend:
+    print("It is the weekend, yay!")
+
+
