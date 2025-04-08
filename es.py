2a64d06438b1b848af5bead05ea5ea1eface3e25
quyenphanlyons <quyenphanlyons@gmail.com>
2025-04-08 20:01:03 +0000

    test1

--- es.py
+++ es.py
@@ -1,8 +1,1 @@
-# I created a file Mobydick.txt in my repository pands-weekly-tasks
-link = "https://github.com/quyenphanlyons/pands-weekly-tasks/blob/main/Mobydick.txt"
-
-# Open file in python - Source stackoverflow.com
-from urllib.request import urlopen
-data = urlopen(link).read().decode('utf-8')
-data.count('e')
 
