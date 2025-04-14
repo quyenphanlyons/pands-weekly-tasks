bb69c75cb8f8b74a22c2b2ea28f2deb00168513f
quyenphanlyons <quyenphanlyons@gmail.com>
2025-04-14 10:15:57 +0000

    plottask improve2

--- plottask.py
+++ plottask.py
@@ -4,8 +4,6 @@
 # Some marks will be given for making the plot look nice (legend etc).
 # Please put a copy of the image of the plot (.png file) into the repository
 
-
-
 # Plotting.
 import matplotlib.pyplot as plt
 # Numerical arrays.
@@ -14,46 +12,35 @@ import numpy as np
 # Group 1: Mean = 5, Std Dev = 2, Size = 1000
 x = np.random.normal(5, 2, 1000)
 
-# Show x.
-print(x)
+# Group 2: 
+# Create y composed by 11 values from 0 to 10 (in my case, I just create 11 value, including 0 and 10, it can be any number of value)
+y = np.linspace(0, 10, 11)
 
-# Histogram.
-plt.hist(x, bins=20, color = 'orange', edgecolor='black', label='Histogram of normal distribution', alpha=0.5)
-
-# Put the copy of the histogram as "plottask histogram" to my repository
-plt.savefig("task8_histogram.png")
-
-
-# Plotting.
-import matplotlib.pyplot as plt
-# Numerical arrays.
-import numpy as np
-
-# Create y composed by 10 value from 0 to 10
-y = np.linspace(0.0, 10.0, 0.1)
-
-# h(x)=x3
+# Create z which is h(y)=y3, I use function pow() to calculate z from y, another way to calculate z is z = y**3
 z = pow(y,3)
 
-# Show y.
-print(y)
-print(z)
+# Histogram of group 1
+plt.hist(x, bins=20, color = 'orange', edgecolor='black', label='Normal distribution mean of 5 and standard deviation of 2', alpha=0.5)
 
-# Plot x vs y.
-plt.plot(y, z, label='z = y3')
+# Plot the fucntion h(y)=y3
+plt.plot(y, z, label='h(x) = x3', color = 'green')
 
-# Add a legend.
+# Add title and labels
+plt.title('Histogram and Function Plot on Same Axes')
+plt.xlabel('x')
+plt.ylabel('Density / h(x)')
 plt.legend()
 
-# Add labels.
-plt.xlabel('y')
-plt.ylabel('$y^3$')
+# put a copy of the image of the plot (.png file) into the repository
+plt.savefig("task8_plot.png")
 
-# Add a title.
-plt.title('function  h(y)=y3')
-
-plt.savefig("task8_plot_funtion.png")
+# Show the plot
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 
 # Resources
 # https://www.w3schools.com/python/matplotlib_histograms.asp
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
+# https://matplotlib.org/stable/gallery/subplots_axes_and_figures/two_scales.html
+
