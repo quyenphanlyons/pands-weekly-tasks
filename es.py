0c0a9975f3a24d536a1b83eff6da882ca2ba7159
quyenphanlyons <quyenphanlyons@gmail.com>
2025-04-15 20:07:02 +0000

    es improve3

--- es.py
+++ es.py
@@ -1,7 +1,35 @@
-# I created a file Mobydick.txt in my repository pands-weekly-tasks
-# Open text file
-with open("moby_dick.txt") as f:
+# Weekly task 7
+# Write a program that reads in a text file and outputs the number of e's it contains. Think about what is being asked here, document any assumptions you are making.
+# The program should take the filename from an argument on the command line. I have not shown you how to do this, you need to look it up.
+# Marks will be given for dealing with errors eg no argument, filename that does not exist, or is not a text file.
+
+
+# I assume that the number of e's means the number of e in both lowercase and uppercase.
+
+# Create a file moby_dick.txt in my repository pands-weekly-tasks
+
+# Create function to read and count a specific letter
+# Function takes 2 parameters: textfile (name of the file needed to be read) and letter (the specific letter needed to be counted) which takes value 'e' by default
+def numletter(textfile, letter = 'e'):
+ # Open text file
+ with open(textfile) as f:
   # Read file
- data = f.read()
-  # Count the number of letter 'e'
- data.count("e")
+  data = f.read()
+  # Count the number of the letter in lowercase
+  data.count(letter)
+  # Count the number of the letter in uppercase
+  letter_uppercase = letter.upper()
+  data.count(letter_uppercase)
+  # Output the number of the letter
+  return data.count(letter) + data.count(letter_uppercase)
+
+
+# Output the number of the letter 'e' in moby_dick.txt
+numletter("moby_dick.txt",)
+
+
+
+# Ressources
+# https://www.w3schools.com/python/ref_string_count.asp
+# https://www.w3schools.com/python/ref_string_upper.asp
+
